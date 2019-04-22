dd99bdbb424e6b257aef63da95db74bbdaab43ab
Sorin Chis <kish.sorin@yahoo.com>
2019-04-22 17:24:06 +0300

    added new image with hover effect

--- animate-app/src/components/ImageComp/Image.jsx
+++ animate-app/src/components/ImageComp/Image.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import cat from "../../assets/cat.jpg";
 import cover from "../../assets/star.jpg";
+import cover2 from "../../assets/logo2.jpg";
 
 const Image = () => {
   return (
@@ -27,6 +28,14 @@ const Image = () => {
             </p>
           </div>
         </WrapperImage1>
+
+        <WrapperImage2>
+          <img src={cover2} alt="" />
+          <div>
+            <h1>Title over here!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </WrapperImage2>
       </Container>
     </Wrapper>
   );
@@ -136,10 +145,10 @@ const ImgWrapper7 = styled.img`
 `;
 
 const WrapperImage1 = styled.div`
-  width: 300px;
+  margin: 20px;
+  max-width: 300px;
   height: 400px;
   position: relative;
-  border: 1px solid black;
   display: flex;
   justify-content: center;
   transition: all 0.5s;
@@ -183,5 +192,54 @@ const WrapperImage1 = styled.div`
   }
   &:hover img {
     transform: scale(1.3) rotate(15deg);
+  }
+`;
+
+const WrapperImage2 = styled.div`
+  margin: 20px;
+  max-width: 300px;
+  height: auto;
+  position: relative;
+  transition: all 0.7s;
+  background: rgba(0, 0, 0, 0);
+  overflow: hidden;
+
+  img {
+    max-width: 100%;
+    transition: all 1s;
+  }
+  div {
+    max-width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    color: white;
+    border: 2px solid purple;
+    opacity: 0;
+    transition: all 0.7s;
+    transform: translateX(-100%);
+
+    h1 {
+      margin: 0;
+      text-transform: uppercase;
+    }
+    p {
+      margin: 0;
+    }
+
+    &:hover {
+      opacity: 1;
+      background: rgba(0, 0, 0, 0.7);
+    }
+  }
+
+  &:hover div {
+    transform: translateX(0);
   }
 `;
