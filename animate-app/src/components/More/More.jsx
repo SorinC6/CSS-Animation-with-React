a8b8c87fcd5ae3a106f2bc7d98b130502ccbb16f
Sorin Chis <kish.sorin@yahoo.com>
2019-04-25 14:26:41 +0300

    added keyfram animation

--- animate-app/src/components/More/More.jsx
+++ animate-app/src/components/More/More.jsx
@@ -5,6 +5,7 @@ const More = () => {
   return (
     <Container>
       <p>More</p>
+      <DivWrapper />
     </Container>
   );
 };
@@ -14,9 +15,29 @@ export default More;
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
+  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
   max-width: 100%;
   background-color: #ed8a63;
   margin: 0 auto;
+`;
+
+const DivWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+  animation-name: moving;
+  animation-duration: 4s;
+  @keyframes moving {
+    0% {
+      transform: translateX(-1000px);
+    }
+    20% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateY(-100px);
+    }
+  }
 `;
